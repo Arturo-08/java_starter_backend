c74bf3b37cbc7f2ef3c87200a3ec4ad8532994da
Juancarcaicedo <74690988+Juancarcaicedo@users.noreply.github.com>
2025-03-17 12:36:57 -0500

    se agrega el proyecto

---
+++ src/test/java/AIVP/Backend_Volunteers/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package AIVP.Backend_Volunteers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
