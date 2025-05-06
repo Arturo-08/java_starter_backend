80f74e4c82c8fe8064f94037247bbda8e0946dcb
Carlos Zamudio <cbuitragoz@unal.edu.co>
2025-05-05 21:10:45 -0500

    Env config files

--- src/main/java/aivp/backend_volunteers/config/security/jwt/JwtUtils.java
+++ src/main/java/aivp/backend_volunteers/config/security/jwt/JwtUtils.java
@@ -1,9 +1,12 @@
 package aivp.backend_volunteers.config.security.jwt;
 
+
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +17,14 @@ import java.util.function.Function;
 @Component
 public class JwtUtils {
 
-    private static final String SECRET_KEY = "THIS IS A SECRET FOR OUR APP - AIVP - 492872810AUTHJWTSECRET29";
+    private final String secretKey;
+
+    public JwtUtils(Environment env) {
+        this.secretKey = env.getProperty("jwt.secret.key");
+    }
 
     private Key getSignInKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        return Keys.hmacShaKeyFor(secretKey.getBytes());
     }
 
     public String extractUsername(String token) {
