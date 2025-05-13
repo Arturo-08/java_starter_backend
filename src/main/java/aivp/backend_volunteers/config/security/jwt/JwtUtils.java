8c0f6ca2b8a2ad6cbf7cd40bae8608ea7b21b573
Carlos Zamudio <cbuitragoz@unal.edu.co>
2025-05-13 10:31:31 -0500

    Deploy initial config

--- src/main/java/aivp/backend_volunteers/config/security/jwt/JwtUtils.java
+++ src/main/java/aivp/backend_volunteers/config/security/jwt/JwtUtils.java
@@ -5,7 +5,6 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
